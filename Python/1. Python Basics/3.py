d1fb1b4c9832e0c2ef449aec572d52d402603a40
akhil3173 <akhilkacham3173@gmail.com>
2025-09-05 13:27:23 +0530

    Create 3.py

---
+++ Python/1. Python Basics/3.py
@@ -0,0 +1,14 @@
+# Defining variables of various data types
+
+int_var = 42                # Integer type
+bool_var = True             # Boolean type, can also be False
+char_var = 'D'              # Character as a string of length 1
+float_var = 3.14            # Floating point number
+double_var = 2.718281828459 # Python float is double-precision
+
+# Printing all the variables with their descriptions
+print("Integer value:", int_var)
+print("Boolean value:", bool_var)
+print("Character value:", char_var)
+print("Float value:", float_var)
+print("Double (float in Python):", double_var)
