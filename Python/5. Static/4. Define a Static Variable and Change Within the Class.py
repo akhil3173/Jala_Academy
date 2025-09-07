543c28de9b23fb22cf48c0344370b9f0c8f2999a
akhil3173 <akhilkacham3173@gmail.com>
2025-09-07 21:14:32 +0530

    Update 4. Define a Static Variable and Change Within the Class.py

--- Python/5. Static/4. Define a Static Variable and Change Within the Class.py
+++ Python/5. Static/4. Define a Static Variable and Change Within the Class.py
@@ -1,6 +1,7 @@
 class D:
-    w = int(input())
-D.w = int(input())
+    w = int(input("Enter initial value for static variable w in class D: "))
+
+D.w = int(input("Enter new value for static variable w modified by class D: "))
 print(D.w)
 d = D()
 print(d.w)
