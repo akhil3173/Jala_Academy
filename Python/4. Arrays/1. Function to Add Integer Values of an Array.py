25a2ff76bb9b9e130259d63fcc5c924c013e2b2c
akhil3173 <akhilkacham3173@gmail.com>
2025-09-07 20:40:43 +0530

    Update 1. Function to Add Integer Values of an Array.py

--- Python/4. Arrays/1. Function to Add Integer Values of an Array.py
+++ Python/4. Arrays/1. Function to Add Integer Values of an Array.py
@@ -1,4 +1,11 @@
-def sum_array(arr):
-    return sum(arr)
-arr = list(map(int, input().split()))
-print(sum_array(arr))
+def array_sum(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+print(array_sum(arr))
