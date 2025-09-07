7bf4df1c0ddc8885e9716e7d07ef142345c07c58
akhil3173 <akhilkacham3173@gmail.com>
2025-09-07 21:35:13 +0530

    Create 1. mypackage_class1.py

---
+++ Python/10.Packages/1. mypackage_class1.py
@@ -0,0 +1,6 @@
+class Class1:
+    def __init__(self):
+        self.value = input("Enter value for Class1: ")
+
+    def method1(self):
+        print("Class1 method called with value:", self.value)
