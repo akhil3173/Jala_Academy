d56a73d52330e57eb4481baa1f1dd4d126c203c6
akhil3173 <akhilkacham3173@gmail.com>
2025-09-07 21:22:14 +0530

    Create 13. Converting to Uppercase and Lowercase (Manual).py

---
+++ Python/6.Strings/13. Converting to Uppercase and Lowercase (Manual).py
@@ -0,0 +1,21 @@
+def to_upper(s):
+    res = ''
+    for c in s:
+        if 'a' <= c <= 'z':
+            res += chr(ord(c) - 32)
+        else:
+            res += c
+    return res
+
+def to_lower(s):
+    res = ''
+    for c in s:
+        if 'A' <= c <= 'Z':
+            res += chr(ord(c) + 32)
+        else:
+            res += c
+    return res
+
+s = input("Enter string: ")
+print(to_upper(s))
+print(to_lower(s))
