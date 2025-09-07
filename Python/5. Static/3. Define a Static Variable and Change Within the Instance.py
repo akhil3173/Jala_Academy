03a527bafdb3208327ba759cd2429ac84272a941
akhil3173 <akhilkacham3173@gmail.com>
2025-09-07 21:14:01 +0530

    Update 3. Define a Static Variable and Change Within the Instance.py

--- Python/5. Static/3. Define a Static Variable and Change Within the Instance.py
+++ Python/5. Static/3. Define a Static Variable and Change Within the Instance.py
@@ -1,6 +1,7 @@
 class C:
-    z = int(input())
+    z = int(input("Enter initial value for static variable z in class C: "))
+
 c = C()
-c.z = int(input())
+c.z = int(input("Enter new value for static variable z modified by instance c: "))
 print(c.z)
 print(C.z)
